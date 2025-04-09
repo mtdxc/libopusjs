31d5f2d86d5ab5949d034a9afc658086f350beb5
cqm <cqm@97kid.com>
2025-04-09 16:48:36 +0800

    fixed build

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # usage: ./build.sh [flags]
+if [ ! -e opus ]; then
+  git clone -b v1.3.1 https://github.com/xiph/opus.git
+  pushd opus
+  ./autogen.sh
+  emconfigure ./configure --disable-rtcd --disable-intrinsics --disable-shared \
+    --enable-static  --disable-stack-protector --disable-hardening
+  emmake make -j$(nproc)
+  popd
+fi
+
 # asm.js release flags: -O2 --memory-init-file 0
 # wasm release flags: -O2 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1
 em++ api.cpp -Iopus/include -Lopus/.libs -lopus --pre-js preapi.js \
